ff45e6ebe32a431f533405ed9cda3bc15575b27c
wowa3090 <wowagamer@gmail.com>
2026-03-11 21:51:22 +0300

    My Place Dimension Mini Update 1.7.0

--- src/main/java/net/wowamod/init/Universe3090ModPaintings.java
+++ src/main/java/net/wowamod/init/Universe3090ModPaintings.java
@@ -15,4 +15,9 @@ import net.minecraft.world.entity.decoration.PaintingVariant;
 public class Universe3090ModPaintings {
 	public static final DeferredRegister<PaintingVariant> REGISTRY = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, Universe3090Mod.MODID);
 	public static final RegistryObject<PaintingVariant> ROSE_MY_PLACE_1 = REGISTRY.register("rose_my_place_1", () -> new PaintingVariant(48, 48));
+	public static final RegistryObject<PaintingVariant> LIGHT_HEART_PAINTING = REGISTRY.register("light_heart_painting", () -> new PaintingVariant(16, 16));
+	public static final RegistryObject<PaintingVariant> P_CPAINTING = REGISTRY.register("p_cpainting", () -> new PaintingVariant(16, 16));
+	public static final RegistryObject<PaintingVariant> PC_DEV_PAINTING = REGISTRY.register("pc_dev_painting", () -> new PaintingVariant(16, 16));
+	public static final RegistryObject<PaintingVariant> PC_DEV_PAINTING_2 = REGISTRY.register("pc_dev_painting_2", () -> new PaintingVariant(16, 16));
+	public static final RegistryObject<PaintingVariant> PC_DEV_PAINTING_3 = REGISTRY.register("pc_dev_painting_3", () -> new PaintingVariant(16, 16));
 }
